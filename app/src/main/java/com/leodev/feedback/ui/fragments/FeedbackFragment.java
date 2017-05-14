227bf0853033b3247b1154c635ce9730a76666f8
Leonid <v666ad2009@gmail.com>
2017-05-15 02:19:26 +0300

    Add gif smile, layout

--- app/src/main/java/com/leodev/feedback/ui/fragments/FeedbackFragment.java
+++ app/src/main/java/com/leodev/feedback/ui/fragments/FeedbackFragment.java
@@ -6,17 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.leodev.feedback.R;
 import com.leodev.feedback.Utils;
 import com.leodev.feedback.event.EventMainChangeFragment;
 
 import org.greenrobot.eventbus.EventBus;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+
 public class FeedbackFragment extends Fragment {
+    @BindView(R.id.ib_bad_smile)
+    ImageView mBadSmile;
+    @BindView(R.id.ib_neutral_smile)
+    ImageView mNeutralSmile;
+    @BindView(R.id.ib_good_smile)
+    ImageView mGoodSmile;
 
     @OnClick({R.id.ib_bad_smile, R.id.ib_neutral_smile, R.id.ib_good_smile})
     public void onClick(View view) {
@@ -46,6 +57,22 @@ public class FeedbackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_smiles, container, false);
         ButterKnife.bind(this, view);
+        initSmiles();
         return view;
+    }
+
+    private void initSmiles(){
+        Glide.with(this)
+                .load(R.drawable.angry).asGif()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+               .into(mBadSmile);
+        Glide.with(this)
+                .load(R.drawable.sarcastic).asGif()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mNeutralSmile);
+        Glide.with(this)
+                .load(R.drawable.clap).asGif()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mGoodSmile);
     }
 }
